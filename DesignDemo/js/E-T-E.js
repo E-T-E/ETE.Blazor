051c423b896df21d391f4f98bfd9939b789faece
Binla <495083320@qq.com>
2022-05-13 17:34:53 +0800

    Change Control

--- DesignDemo/js/E-T-E.js
+++ DesignDemo/js/E-T-E.js
@@ -1,13 +1,45 @@
 var container;
 var camera, scene, renderer;
-var orbitControls, clock, delta;
-let mouseX = 0;
-let mouseY = 0;
-let windowHalfX = window.innerWidth / 2;
-let windowHalfY = window.innerHeight / 2;
+
+var sphereMesh;
+
+var mouse = new THREE.Vector2;
+
+var me = false
+var isStop = false;
+var we = true;
+
+var viewX = 0;
+var be = 0;
+var x = 500;
+var j = 1e3;
+
+var PI_HALF = Math.PI / 2;
+
+var pos = {
+    x: 0,
+    y: 0
+};
+var rotation = {
+    x: 0,
+    y: 0
+};
+var target = {
+  x: 2.1 * Math.PI,
+  y: Math.PI / 4.5
+};
+var p = {
+    x: 0,
+    y: 0
+};
 
 main();
 sphere();
+var a = 6.98;
+addSharp(a, a, 15 * a, 0, 0, "Binla");
+var b = 15;
+addSharp(100, 0, 0, 180, 90, "Test");
+
 onWindowResize();
 animate();
 
@@ -25,20 +57,20 @@ function main(){
     camera = new THREE.PerspectiveCamera(30,
                   window.innerWidth/window.innerHeight, 1, 1000);
 
-    var dLight = new THREE.DirectionalLight(0xffffff, 0.8);
-    dLight.position.set(-800, 2000, 400);
+    var dLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    dLight.position.set(-1000, 100, 100);
     camera.add(dLight);
     
-    var dLight1 = new THREE.DirectionalLight(0x7982f6, 1);
-    dLight1.position.set(-200, 500, 200);
+    var dLight1 = new THREE.DirectionalLight(0x7982f6, 1.5);
+    dLight1.position.set(-2000, 2500, 1500);
     camera.add(dLight1);
     
-    var dLight2 = new THREE.PointLight(0x8566cc, 0.5);
+    var dLight2 = new THREE.PointLight(0x8566cc, 0.05);
     dLight2.position.set(-200, 500, 200);
     camera.add(dLight2);
 
     camera.position.set(0, 0, 400);//设置相机位置
-    camera.lookAt(new THREE.Vector3(0,0,400));//让相机指向原点
+    camera.lookAt(new THREE.Vector3(0,0,0));//让相机指向原点
     scene.add(camera);
 
     //添加雾特效
@@ -50,71 +82,128 @@ function main(){
     renderer.setSize(window.innerWidth, window.innerHeight);//设置窗口尺寸
     //将renderer关联到container，这个过程类似于获取canvas元素
     container.appendChild(renderer.domElement);
-    //添加轨道控制器
-    //新建一个轨道控制器
-    orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
-    //添加阻尼感
-    orbitControls.enableDamping = true;
-    orbitControls.dynamicDampingFactor = 0.01;
-    //禁用摄像机平移
-    orbitControls.enablePan = false;
-    //配置缩放范围
-    orbitControls.minDistance = 200;
-    orbitControls.maxDistance = 500;
-    //旋转速度
-    orbitControls.rotateSpeed = 0.08;
-    //缩放速度
-    orbitControls.zoomSpeed = 1;
-    //关闭自动旋转
-    orbitControls.autoRotate = false;    
-    //垂直旋转的角度的上\下限
-    orbitControls.minPolarAngle = Math.PI / 3.5;
-    orbitControls.maxPolarAngle = Math.PI - Math.PI / 3;
 
+    container.addEventListener("mousedown", onMouseDown, false);
+    container.addEventListener("mouseleave", onMouseOut, false);
+    container.addEventListener("mousemove", onMouseMove, false);
+    container.addEventListener("touchmove", move, false);
     window.addEventListener('resize', onWindowResize, false);
-    document.addEventListener("mousemove", onMouseMove);
 }
 
 //创建一个球
 function sphere(){
     //创建球体
-    var sphereGeo = new THREE.SphereGeometry(60, 40, 40);
-
+    var sphereGeo = new THREE.SphereGeometry(100, 40, 40);
     //创建材质
-    const textureLoader = new THREE.TextureLoader()
-    const texture = textureLoader.load('https://cdn.binla.site/world.jpg');
-    var sphereMat = new THREE.MeshLambertMaterial({
-        map: texture,
-        overdraw: .5,
-        transparent: true,
-        side: THREE.DoubleSide
-    });
-
-    var sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);//创建球体网格模型
+    var sphereMat = new THREE.MeshLambertMaterial({color: 0x3b1066});
+    sphereMesh = new THREE.Mesh(sphereGeo, sphereMat);//创建球体网格模型
     sphereMesh.position.set(0, 0, 0);//设置球的坐标
     scene.add(sphereMesh);//将球体添加到场景
 }
 
-function onMouseMove(event) {
-    mouseX = event.clientX - windowHalfX;
-    mouseY = event.clientY - windowHalfY;
-    // console.log("x: " + mouseX + " y: " + mouseY);
-}
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    windowHalfX = window.innerWidth / 1.5;
-    windowHalfY = window.innerHeight / 1.5;
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
-function animate() {
-    camera.position.x +=
-      Math.abs(mouseX) <= windowHalfX / 2
-        ? (mouseX / 2 - camera.position.x) * 0.005
-        : 0;
-    camera.position.y += (-mouseY / 2 - camera.position.y) * 0.005;
-    camera.lookAt(scene.position);
-    orbitControls.update();
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
+
+function onMouseDown(event) {
+    event.preventDefault();
+    me = true;
+    container.addEventListener("mouseup", onMouseUp, false);
+    pos.x = -event.clientX;
+    pos.y = event.clientY;
+    p.x = target.x;
+    p.y = target.y;
+    container.style.cursor = "move";
 }
+var onMouseOut = function () {
+    onMouseUp();
+};
+function onMouseUp() {
+    me = false;
+    container.removeEventListener("mouseup", onMouseUp, false);
+    container.style.cursor = "auto";
+}
+function onMouseMove(event) {
+    if (me) {
+      mouse.x = -event.clientX;
+      mouse.y = event.clientY;
+      var t = j / 1e3;
+      target.x = p.x + .005 * (mouse.x - pos.x) * t;
+      target.y = p.y + .005 * (mouse.y - pos.y) * t;
+      target.y = target.y > PI_HALF ? PI_HALF : target.y;
+      target.y = target.y < -PI_HALF ? -PI_HALF : target.y;
+    } else {
+      update(event);
+    }
+}
+function update(event) {
+}
+function move(event) {
+    if (event.preventDefault(), me) {
+      var touches = event.changedTouches;
+      var i = 0;
+      for (; i < touches.length; i++) {
+        mouse.x = -touches[ i ].pageX;
+        mouse.y = touches[ i ].pageY;
+      }
+      var t = j / 1e3;
+      target.x = p.x + .005 * (mouse.x - pos.x) * t;
+      target.y = p.y + .005 * (mouse.y - pos.y) * t;
+      target.y = target.y > PI_HALF ? PI_HALF : target.y;
+      target.y = target.y < -PI_HALF ? -PI_HALF : target.y;
+    }
+    return false;
+}
+
+function applyForEach(viewX) {
+    x = x - viewX;
+    x = x > 1e3 ? 1e3 : x;
+    x = x < 350 ? 350 : x;
+}
+
+function animate() {
+    if (!isStop) {
+        applyForEach(viewX);
+        if (we) {
+            if (be === target.x + target.y) {
+              target.x -= .005;
+            } else {
+              be = target.x + target.y;
+            }
+            rotation.x += .1 * (target.x - rotation.x);
+            rotation.y += .1 * (target.y - rotation.y);
+            j = j + .3 * (x - j);
+            camera.position.x = j * Math.sin(rotation.x) * Math.cos(rotation.y);
+            camera.position.y = j * Math.sin(rotation.y);
+            camera.position.z = j * Math.cos(rotation.x) * Math.cos(rotation.y);
+        }
+        camera.lookAt(sphereMesh.position);
+        renderer.render(scene, camera);
+        requestAnimationFrame(animate);
+    }
+}
+
+function addSharp(xPosition, yPosition, zPosition, rotationX, rotationY, title) {
+    const x = 0, y = 0;
+
+    const heartShape = new THREE.Shape();
+    heartShape.moveTo( x + 5, y + 5 );
+    heartShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
+    heartShape.bezierCurveTo( x - 6, y, x - 6, y + 7,x - 6, y + 7 );
+    heartShape.bezierCurveTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
+    heartShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
+    heartShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
+    heartShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
+
+    const geometry = new THREE.ShapeGeometry( heartShape );
+    const material = new THREE.MeshBasicMaterial( { color: 0xe13304 } );
+    
+    var r = new THREE.Mesh(geometry, material);
+    r.position.set(xPosition, yPosition, zPosition);
+    r.rotation.x = THREE.Math.degToRad(rotationX);
+    r.rotation.y = THREE.Math.degToRad(rotationY);
+    r.name = "destory-area target=" + title;
+    scene.add(r);
+  }
